f4be2b97d3141ced7a6720cf358ef78790329fe6
miyazawa-38 <takuya.miyazawa38@gmail.com>
2026-06-29 16:10:55 +0900

    remove size from function to main

--- main.m
+++ main.m
@@ -1,7 +1,21 @@
+%% main関数
+
+% % 隣接行列
+% A = [0,3;
+%      1,0];
+
 % 隣接行列
-A = [0,3;
-     1,0];
+s_1 = 0.0742;
+s_2 = 0;
+num_nodes = 10;
+[A,f,x] = randomPlot(s_1, s_2, num_nodes);
 
 % グラフ作用素を算出する関数
-[H, D, L, L_] = getGraphOperator(A)
-[U,Lambda] = eig(L_)
+[H, D, L, L_] = getGraphOperator(A);
+[U,Lambda] = eig(L_);
+
+disp(A);
+disp(H);
+disp(D);
+disp(U);
+disp(Lambda);
--- randomPlot.m
+++ randomPlot.m
@@ -1,37 +1,37 @@
-%% 鎌田さんのランダムプロットそのまま
+%% 鎌田さんのランダムプロット内容のまま（コードは変更）
 
-function [w,f,x] = randplot(s_1,s_2)
-size = 100;
-rng(0)
-x = 2*rand(2,size);
-%scatter(x(1,:),x(2,:));
-w = zeros(size);
-for n=1:size
-    for m=1:size
-        if m > n
-            if (norm(x(:,m) - x(:,n)))^2 <= 0.6
-                w(m,n) = exp(-((norm(x(:,m) - x(:,n)))^2)/2 * s_1 * s_1);
-            end
-        end
-        if m < n
-            w(m,n) = s_2 * w(n,m);
-        end
-    end
-end
-f = ones(1,size);
-
-for m=1:size
-    if x(1,m)>1
-        if x(2,m)>1
-            f(m) = -1;
-        end
-    end
-
-    if x(1,m)<1
-        if x(2,m)<1
-            f(m) = -1;
-        end
-    end
+function [A,f,x] = randomPlot(s_1, s_2, num_nodes)
+  rng(0)
+  x = 2*rand(2,num_nodes);
+  %scatter(x(1,:),x(2,:));
+  A = zeros(num_nodes);
+  for n=1:num_nodes
+      for m=1:num_nodes
+          if m > n
+              if (norm(x(:,m) - x(:,n)))^2 <= 0.6
+                  A(m,n) = exp(-((norm(x(:,m) - x(:,n)))^2)/2 * s_1 * s_1);
+              end
+          end
+          if m < n
+              A(m,n) = s_2 * A(n,m);
+          end
+      end
+  end
+  f = ones(1,num_nodes);
+  
+  for m=1:num_nodes
+      if x(1,m)>1
+          if x(2,m)>1
+              f(m) = -1;
+          end
+      end
+  
+      if x(1,m)<1
+          if x(2,m)<1
+              f(m) = -1;
+          end
+      end
+  end
 end
 
 %f = ceil(rand(1,500)-0.5)*2 - 1;
